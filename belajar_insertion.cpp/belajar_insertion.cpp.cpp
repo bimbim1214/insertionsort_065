860ad7ba3eefbd2012a9cd225119c518340b9380
bimbim1214 <bimoadityapangestu@gmail.com>
2023-03-24 15:26:04 +0700

    menambahkan void input dan memasukkan jumlah data pada array

--- belajar_insertion.cpp/belajar_insertion.cpp.cpp
+++ belajar_insertion.cpp/belajar_insertion.cpp.cpp
@@ -3,3 +3,23 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+	while (true) {
+
+		cout << "masukan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\narray yang anda masukan maksimal 20 elemen.\n";
+		}
+
+	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "masukan element array" << endl;
+	cout << "=====================" << endl;
