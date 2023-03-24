71cc16f996548c135c5bb9b70a670c92a9479dd0
bimbim1214 <bimoadityapangestu@gmail.com>
2023-03-24 15:31:35 +0700

    menggunakan perulangan for untuk menyimpan i

--- belajar_insertion.cpp/belajar_insertion.cpp.cpp
+++ belajar_insertion.cpp/belajar_insertion.cpp.cpp
@@ -23,3 +23,10 @@ void input() {
 	cout << "=====================" << endl;
 	cout << "masukan element array" << endl;
 	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
