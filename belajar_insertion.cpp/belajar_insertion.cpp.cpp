ff0a9cf4a3f2ba12da891bc8e71a9d30b15d95f9
bimbim1214 <bimoadityapangestu@gmail.com>
2023-03-24 15:46:15 +0700

    melakukan pengulangan j di display dengan mengngunakan for

--- belajar_insertion.cpp/belajar_insertion.cpp.cpp
+++ belajar_insertion.cpp/belajar_insertion.cpp.cpp
@@ -60,4 +60,8 @@ void display() {
 	cout << "element array yang telah tersusun" << endl;
 	cout << "=================================" << endl;
 
-	
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
+	}
+}
+
